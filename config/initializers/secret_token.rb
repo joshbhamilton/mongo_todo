d089e37f90209e6fb2d8a009c648d6d6a94e7b7e
Josh Hamilton <joshb.hamilton@gmail.com>
2011-03-15 09:57:28 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoTodo::Application.config.secret_token = '9e1c59fadcace24566c05fdd0bcfccad850e6601dbd7654c96a0a23493e4c3d11dd3866d3aba118a0b69da86bb0ea195a38ff113ef617aa78e63be79e5336fa4'
