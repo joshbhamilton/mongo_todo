6e95639000820f7455f3ead4ab05930fdfaf0a36
Josh Hamilton <joshb.hamilton@gmail.com>
2011-03-16 13:13:30 -0700

    revised initializer

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,1 +1,3 @@
-MongoMapper.database = "todo-#{Rails.env}"
+MongoMapper.connection = Mongo::Connection.new('ec2-204-236-177-224.us-west-1.compute.amazonaws.com', 27017)
+MongoMapper.database = "#{app_name}_#{node[:environment][:framework_env]}"
+MongoMapper.database.authenticate("root", "#{user[:password]}")
