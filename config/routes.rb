875806381f5c78aefbb39fa4e3d95fe91a76da99
Josh Hamilton <joshb.hamilton@gmail.com>
2011-03-16 16:49:07 -0700

    changed homepage

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ MongoTodo::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => "project#index"
+  root :to => "projects#index"
 
   # See how all your routes lay out with "rake routes"
 
